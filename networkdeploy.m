82f22c9194db93ce61d29049988dd621d531cefb
XINLIU <xinliu1@umbc.edu>
2018-03-07 09:26:07 -0500

    MatLab Cell

--- networkdeploy.m
+++ networkdeploy.m
@@ -1,8 +1,21 @@
 function y = networkdeploy(number, size, range)
 
 position = rand(number,2)*size - size/2; 
+pcopy = posiion;
 
-if
+left = size(position,1);
+paraentlayer = [];
+childlayer = [];
+
+while left ~= 0 
+    for i = 1 : 1 : size(paraentlayer, 1)
+        if postion(i,1)^2 + position(i,2)^2 <= range^2
+            layer = [layer; position(i,:)];
+            pcopy(i, :) = [];
+        
+        end
+    end
+end
 
 
 end
