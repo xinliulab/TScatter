7482eebe273dc7c36fea87c035ad364e42a556b3
LIUXIN <liuxin0123@foxmail.com>
2018-03-07 22:38:35 -0500

    Cell Array

--- networkdeploy.m
+++ networkdeploy.m
@@ -3,14 +3,15 @@ function y = networkdeploy(number, range, scale)
 parent = [0];
 position = [[1:number]',rand(number,2)*range - range/2]; 
 child = position;
-route = {};
+route = {parent};
 numlayer = 0;
 
 while size(child,1) ~= 0
-    flag = 1;%
+%     flag = 1;
     d = [];
+    temp_route = {};
     for i = 1 : 1 : size(parent, 1)
-        p_id = parent(i,end);%%%%
+        p_id = parent(i,end);
         if p_id == 0
             p_x = 0;
             p_y = 0;
@@ -27,18 +28,15 @@ while size(child,1) ~= 0
             if (c_x - p_x)^2 + (c_y - p_y)^2 <= scale^2 
                 temp_route = {cell2mat(temp_route);[p_id,c_id]};
             end             
-                flag = flag + 1;%
-                d = [d;j];
-
         end
     end
-    parent = route;
-    for p = 1 : 1 : size(d,1)
-        child(d(p,:)-p+1,:) = [];
-    end
+%     for n = 1 : 1 : size(temp_route,1)
+    
+    parent = temp_route;
+%     for p = 1 : 1 : size(d,1)
+%         child(d(p,:)-p+1,:) = [];
+%     end
 
 end
-
-y = route;
 
 end
